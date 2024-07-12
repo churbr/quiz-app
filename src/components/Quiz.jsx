c1f795a1bf9983a058ed099510ab7f5e1d10948e
Bryan Chu <chu.reybr@gmail.com>
2024-07-12 23:45:32 +0800

    Shuffling Answers & Adding Quiz Logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import QUESTIONS from '../questions';
+import quizCompletedImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
   // const [questionIndex, setQuestionIndex] = useState(0);
@@ -7,6 +8,25 @@ export default function Quiz() {
 
   // Derived value, instead of using another state
   const activeQuestionIndex = userAnswer.length;
+
+  const quizCompleted = activeQuestionIndex === QUESTIONS.length;
+
+  if (quizCompleted) {
+    return (
+      <div id="summary">
+        <img src={quizCompletedImg} alt="Trophy" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  // We need to create a new array because we don't want to change the original array
+  // Because in the original array contains the correct value, which is always the first index
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+
+  // To shuffle, negative number
+  // To retain its original order any positive number
+  shuffledAnswers.sort(() => Math.random - 0.5);
 
   const handleUserAnswer = (answer) => {
     setUserAnswer((prevUserAnswers) => {
@@ -19,7 +39,7 @@ export default function Quiz() {
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li className="answer" key={answer}>
               <button onClick={() => handleUserAnswer(answer)}>{answer}</button>
             </li>
