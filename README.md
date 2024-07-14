34ba5d4695f4942888db6f2b587481eb5e0d71cf
Bryan Chu <chu.reybr@gmail.com>
2024-07-14 16:20:20 +0800

    Added Clean Up functions on setTimeout and setInterval

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 - Functions, in JavaScript are values, they are `object` type of values. And when a React component is re-rendered, a new object in memory is being created. And the function/object that was created before is not the same function created now. It is stored to a different location in memory. Hence, a new value in memory.
 - In useState() hook, the state updating function returned by useState does not change on re-renders of the component. React guarantees that the state updating function returned by useState will always refer to the same function
--
+- useCallback() hook allows you to memoize a function instance between renders. It returns a memoized version of the callback function that only changes if one of the dependencies has changed. This is particularly useful when passing callbacks to child components to prevent unnecessary re-renders
--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -10,7 +10,16 @@ export default function QuestionTimer({ timeout, onTimeout }) {
    **/
   useEffect(() => {
     console.log('timeout');
-    setTimeout(onTimeout, timeout);
+    const timer = setTimeout(onTimeout, timeout);
+
+    // Clean up function for setTimeout: This will automatically be executed by React
+    // Before it runs the effect funtion or when this component is unmounted/removed from the DOM
+    // We have to clean up the existing timer if this effect function is going to run again
+    // So, that it doesn't matter if this function is called multiple times. (For example: StrictMode)
+    // Because when we always clean up the old timer, we will only have one timer instance running always
+    return () => {
+      clearTimeout(timer);
+    };
   }, [timeout, onTimeout]);
   /**
    * 4) Now, we have to add `timeout`, and `onTimeout` as array dependencies. Since we're using those 2 props inside the useEffect()
@@ -24,12 +33,21 @@ export default function QuestionTimer({ timeout, onTimeout }) {
    **/
   useEffect(() => {
     console.log('interval');
-    setInterval(() => {
+    const interval = setInterval(() => {
       /**
        * 1) Since we have a state function call here, everytime this function is called it will re-evaluate/render the component.
        **/
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
+
+    // Clean up function for setInterval: This will automatically be executed by React
+    // Before it runs the effect funtion or when this component is unmounted/removed from the DOM
+    // We have to clean up the existing interval if this effect function is going to run again
+    // So, that it doesn't matter if this function is called multiple times. (For example: StrictMode)
+    // Because if we always clean up the old intervals, we will only have one interval running
+    return () => {
+      clearInterval(interval);
+    };
   }, []); // dependency array are props, state, and all the variables and functions declared directly inside your component body
 
   return <progress id="question-time" max={timeout} value={remainingTime} />;
